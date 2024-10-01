c3bc8e1e2d2469ba0cb633c2f686c30d058a8547
mariam abulail <mariamabulail88@gmail.com>
2024-10-01 15:46:29 +0300

    'new'

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
     console.error(error);
   }); */
 
-  async function fetchData() {
+  const fetchData =async()=> {
     try {
       const pikachuName = document
         .getElementById("pikachuName")
@@ -27,59 +27,60 @@
     }
   }
   
-  async function f11() {
-    let url =
+  const f11=async  ()=> {
+    const url =
       "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits";
-    let response = await fetch(url);
+    const response = await fetch(url);
   
-    let commits = await response.json(); // read response body and parse as JSON
+    const commits = await response.json(); // read response body and parse as JSON
   
     /* alert(commits[0].author.login);
      */
   }
   f11();
   
-  let head = document.getElementById("head");
-  head.onclick = function () {
+  const head = document.getElementById("head");
+  head.onclick =  ()=> {
     head.classList.toggle("color");
   };
-  let open = document.getElementById("open");
-  let close = document.getElementById("close");
-  let nav = document.querySelector(".nav");
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const nav = document.querySelector(".nav");
   
-  open.onclick = function () {
+  open.onclick =  () => {
     nav.classList.remove("hide");
     close.classList.remove("hide");
     open.classList.add("hide");
   };
-  close.onclick = function () {
+  close.onclick =  ()=> {
     nav.classList.add("hide");
     close.classList.add("hide");
     open.classList.remove("hide");
   };
-  let ul = document.querySelector(".ul1");
-  let p = document.getElementById("p11");
-  p.onclick = function () {
+  const ul = document.querySelector(".ul1");
+  const p = document.getElementById("p11");
+  p.onclick =  ()=> {
     ul.classList.toggle("hide");
   };
   
-  document.querySelector(".menu").onclick = function (event) {
+  document.querySelector(".menu").onclick = event=> {
     if (event.target.nodeName != "A") return;
   
-    let href = event.target.getAttribute("href");
+    const href = event.target.getAttribute("href");
     /* alert(href); */
   };
   
-  let btn1 = document.getElementById("btn1");
-  let btn2= document.getElementById("btn2");
-  window.onscroll = function () {
+  const btn1 = document.getElementById("btn1");
+  /* btn1='mariam'; cause an error because its a const variable */
+  const btn2= document.getElementById("btn2");
+  window.onscroll = ()=> {
     if (scrollY > 400) {
       btn1.style.display = "block";
     } else {
       btn1.style.display='none';
     }
   };
-  /* window.onscroll = function () {
+  /* window.onscroll =  ()=> {
       if (scrollX > 100) {
         btn2.style.display = "block";
       } else {
@@ -87,7 +88,7 @@
       }
     };
     */ 
-  btn1.onclick = function () {
+  btn1.onclick =  ()=> {
       scroll({
           top:0,
           left:0,
@@ -96,7 +97,7 @@
       })
   };
   
-  /* btn2.onclick = function () {
+  /* btn2.onclick =  ()=> {
       scroll({
           top:0,
           left:0,
@@ -109,7 +110,7 @@
       document.querySelector('.colorDiv').style.backgroundColor=sessionStorage.color;
   }
   
-  function getColo(color){
+ const getColo = color=>{
       sessionStorage.setItem('color',color)
       document.querySelector('.colorDiv').style.backgroundColor = color;
   
@@ -123,10 +124,16 @@
   /* sessionStorage.clear();
    */
   
-  let txt=document.getElementById('txt');
+  const txt=document.getElementById('txt');
   if(sessionStorage.length>0){
       txt.value=sessionStorage.getItem('txt');
   }
-  txt.onkeyup=function(){
+  txt.onkeyup=()=>{
       sessionStorage.setItem('txt',txt.value);
   }
+  document.cookie = "color=red; expires=Wed, 02 Oct 2024 13:43:41 GMT;";
+  //to delete it : 1-delete the value // 2-put date in past  
+  document.cookie = "color=red; expires=Wed, 02 Feb 2024 13:43:41 GMT;";
+  //with path to access ot from this path
+  document.cookie = "color=red; expires=Wed, 10 Oct 2024 13:43:41 GMT; path=/";
+  console.log(document.cookie);
